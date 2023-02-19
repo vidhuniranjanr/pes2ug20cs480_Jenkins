bc4eb27b9ab847234c8fa5a9c3a992db54b78899
vidhu_niranjan313 <91695148+vidthedon@users.noreply.github.com>
2023-02-19 19:53:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello there, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
